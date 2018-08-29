988a054998855d15cfc76739093b62f88777c203
Huynh Duc <huynhduc.cntt@gmail.com>
2018-08-29 13:05:27 +0700

    Init Project Website Vue with Sass Struct Base

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Vue with sass</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: ' Home Vue'
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit SCSS to this component only -->
+<style lang="scss" scoped>
+  @import "~@/style/index.scss";
+</style>
