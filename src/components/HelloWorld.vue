601d187a93aab9ee7434c604779865b3b4765e93
Huynh Duc <huynhduc.cntt@gmail.com>
2018-09-04 20:32:40 +0700

    remote default template of vue cli

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Vue with sass</h2>
-  </div>
+ <div>hhhihi</div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: ' Home Vue'
-    }
-  }
-}
+// export default {
+//   name: "HelloWorld",
+//   data() {
+//     return {
+//       msg: " Home Vue"
+//     };
+//   }
+// };
 </script>
 <!-- Add "scoped" attribute to limit SCSS to this component only -->
 <style lang="scss" scoped>
-  @import "~@/style/index.scss";
+@import "~@/style/index.scss";
 </style>
